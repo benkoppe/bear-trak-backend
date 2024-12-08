fd9e6bf0fb5629661d25a2c796909d7f3a03c8e3
Ben <Koppe.Development@gmail.com>
2024-12-08 06:29:16 -0500

    Add util functions to static gym types

--- gyms/static/types.go
+++ gyms/static/types.go
@@ -1,5 +1,12 @@
 package static
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
 type Gym struct {
 	ID         int         `json:"id"`
 	Name       string      `json:"name"`
@@ -26,9 +33,11 @@ type Equipment struct {
 	Items []string `json:"items"`
 }
 
+type TimeString string
+
 type Hours struct {
-	Open  string `json:"open"`
-	Close string `json:"close"`
+	Open  TimeString `json:"open"`
+	Close TimeString `json:"close"`
 }
 
 type WeekHours struct {
@@ -39,4 +48,62 @@ type WeekHours struct {
 	Friday    []Hours `json:"friday"`
 	Saturday  []Hours `json:"saturday"`
 	Sunday    []Hours `json:"sunday"`
+}
+
+func (w WeekHours) GetHours(date time.Time) []Hours {
+	switch date.Weekday() {
+	case time.Monday:
+		return w.Monday
+	case time.Tuesday:
+		return w.Tuesday
+	case time.Wednesday:
+		return w.Wednesday
+	case time.Thursday:
+		return w.Thursday
+	case time.Friday:
+		return w.Friday
+	case time.Saturday:
+		return w.Saturday
+	case time.Sunday:
+		return w.Sunday
+	default:
+		return nil
+	}
+}
+
+func (t TimeString) parseTime() (struct {
+	Hour   int
+	Minute int
+}, error,
+) {
+	parts := strings.Split(string(t), ":")
+	emptyTime := struct {
+		Hour   int
+		Minute int
+	}{Hour: 0, Minute: 0}
+
+	if len(parts) != 2 {
+		return emptyTime, fmt.Errorf("invalid time format: %s", t)
+	}
+	hours, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return emptyTime, fmt.Errorf("invalid hours: %s", parts[0])
+	}
+	minutes, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return emptyTime, fmt.Errorf("invalid minutes: %s", parts[1])
+	}
+
+	return struct {
+		Hour   int
+		Minute int
+	}{Hour: hours, Minute: minutes}, nil
+}
+
+func (t TimeString) ToDate(date time.Time) (time.Time, error) {
+	parsed, err := t.parseTime()
+	if err != nil {
+		return time.Time{}, err
+	}
+	return time.Date(date.Year(), date.Month(), date.Day(), parsed.Hour, parsed.Minute, 0, 0, date.Location()), nil
 }
