544cd62ffbae0fc2b2e5fe7a03fcc67015f4576c
Ben <Koppe.Development@gmail.com>
2024-12-08 02:33:59 -0500

    Add support for static files

--- main/main.go
+++ main/main.go
@@ -9,12 +9,26 @@ import (
 )
 
 func main() {
+	// create main service
 	service := &handler.BackendService{}
 	srv, err := backend.NewServer(service)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := http.ListenAndServe(":3000", srv); err != nil {
+
+	// create multiplexer to handle service and static file routes
+	mux := http.NewServeMux()
+
+	// mount the API
+	mux.Handle("/", srv)
+
+	// serve static files from "./static" under the "/static" route
+	staticDir := "./static"
+	fileServer := http.FileServer(http.Dir(staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// start the server
+	if err := http.ListenAndServe(":3000", mux); err != nil {
 		log.Fatal(err)
 	}
 }
