d5227110c015bbf365a5e97fdf70b99d03398bd2
Ben <Koppe.Development@gmail.com>
2024-12-09 00:21:19 -0500

    Implement bus routes in handler

--- handler/handler.go
+++ handler/handler.go
@@ -6,6 +6,7 @@ import (
 	backend "github.com/benkoppe/bear-trak-backend/backend"
 	"github.com/benkoppe/bear-trak-backend/dining"
 	"github.com/benkoppe/bear-trak-backend/gyms"
+	"github.com/benkoppe/bear-trak-backend/transit"
 )
 
 type BackendService struct{}
@@ -16,6 +17,10 @@ func (bs *BackendService) GetV1Dining(ctx context.Context) ([]backend.Eatery, er
 
 func (bs *BackendService) GetV1Gyms(ctx context.Context) ([]backend.Gym, error) {
 	return gyms.Get("https://connect2concepts.com/connect2/?type=bar&key=355de24d-d0e4-4262-ae97-bc0c78b92839&loc_status=false")
+}
+
+func (bs *BackendService) GetV1TransitRoutes(ctx context.Context) ([]backend.BusRoute, error) {
+	return transit.GetRoutes("https://s3.amazonaws.com/tcat-gtfs/tcat-ny-us.zip")
 }
 
 func (bs *BackendService) NewError(ctx context.Context, err error) *backend.ErrorStatusCode {
