dbbbd551fad575b5174bf754c77dd4485dccaf94
Ben <Koppe.Development@gmail.com>
2025-02-16 03:25:44 -0500

    Add cbord base url to external routes

--- go-server/handler/external_routes.go
+++ go-server/handler/external_routes.go
@@ -3,6 +3,8 @@ package handler
 const (
 	eateriesUrl = "https://now.dining.cornell.edu/api/1.0/dining/eateries.json"
 
+	cbordBaseUrl = "https://services.get.cbord.com/GETServices/services/json"
+
 	gymCapacitiesUrl = "https://goboardapi.azurewebsites.net/api/FacilityCount/GetCountsByAccount?AccountAPIKey=355de24d-d0e4-4262-ae97-bc0c78b92839"
 
 	availtecUrl = "https://realtimetcatbus.availtec.com/InfoPoint/rest"
