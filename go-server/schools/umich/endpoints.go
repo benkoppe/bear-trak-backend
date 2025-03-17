44a5deabee929b6660359751447f2aed907ec995
Ben <Koppe.Development@gmail.com>
2025-03-17 03:54:20 -0400

    Add bustime url to endpoints

--- go-server/schools/umich/endpoints.go
+++ go-server/schools/umich/endpoints.go
@@ -2,4 +2,8 @@ package umich
 
 const (
 	eateriesBaseUrl = "https://dining.umich.edu/menus-locations"
+
+	bustimeUrl = "https://mbus.ltp.umich.edu/bustime/api/v3"
+
+	gtfsStaticUrl = "https://webapps.fo.umich.edu/transit_uploads/google_transit.zip"
 )
