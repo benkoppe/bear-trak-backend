b529fa781e79dc33dc85ca199583bd932dd46924
Ben <Koppe.Development@gmail.com>
2025-04-08 18:49:34 -0400

    Add alerts for Harvard

---
+++ go-server/alerts/harvard/alerts.go
@@ -0,0 +1,15 @@
+package harvard
+
+import (
+	_ "embed"
+
+	"github.com/benkoppe/bear-trak-backend/go-server/alerts"
+	"github.com/benkoppe/bear-trak-backend/go-server/api"
+)
+
+//go:embed alerts.json
+var alertBytes []byte
+
+func Get() ([]api.Alert, error) {
+	return alerts.Get(alertBytes)
+}
