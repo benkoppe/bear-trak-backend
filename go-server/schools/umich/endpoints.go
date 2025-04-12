92dddc493796203186529e78cb0b2109e6ef2b8d
Ben <Koppe.Development@gmail.com>
2025-04-11 22:14:32 -0400

    Implement umich cbord integration ('dining user')

--- go-server/schools/umich/endpoints.go
+++ go-server/schools/umich/endpoints.go
@@ -3,6 +3,8 @@ package umich
 const (
 	eateriesBaseUrl = "https://dining.umich.edu/menus-locations"
 
+	cbordInstitutionId = "25b49373-032d-4b08-ae72-307f54ec6f56"
+
 	bustimeUrl = "https://mbus.ltp.umich.edu/bustime/api/v3"
 
 	gtfsStaticUrl = "https://webapps.fo.umich.edu/transit_uploads/google_transit.zip"
