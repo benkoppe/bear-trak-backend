95e5b745a799343faecea83ef8ea0e96b67bb848
Ben <Koppe.Development@gmail.com>
2025-04-18 17:39:25 -0400

    Add last stop and next stop to GTFS-RT vehicle parser

--- go-server/transit/shared/gtfs_rt/convert_vehicles.go
+++ go-server/transit/shared/gtfs_rt/convert_vehicles.go
@@ -2,6 +2,7 @@ package gtfs_rt
 
 import (
 	"log"
+	"sort"
 
 	"github.com/benkoppe/bear-trak-backend/go-server/api"
 	"github.com/benkoppe/bear-trak-backend/go-server/utils"
@@ -15,33 +16,80 @@ func ConvertVehicle(vehicle gtfs.Vehicle, staticGtfs gtfs.Static, realtimeGtfs g
 		return t.ID == tripId.ID
 	})
 
+	apiVehicle := api.Vehicle{
+		ID:            api.NewStringVehicleID(vehicleId.ID),
+		LastUpdated:   *vehicle.Timestamp,
+		DisplayStatus: "",
+		LastStop:      api.NilString{Null: true},
+	}
+
+	if vehicle.Position.Bearing == nil {
+		log.Printf("failed to find bearing for vehicle ID: %s", vehicleId)
+	} else {
+		apiVehicle.Heading = int(*vehicle.Position.Bearing)
+	}
+	if vehicle.Position.Latitude == nil {
+		log.Printf("failed to find latitude for vehicle ID: %s", vehicleId)
+	} else {
+		apiVehicle.Latitude = float64(*vehicle.Position.Latitude)
+	}
+	if vehicle.Position.Longitude == nil {
+		log.Printf("failed to find longitude for vehicle ID: %s", vehicleId)
+	} else {
+		apiVehicle.Longitude = float64(*vehicle.Position.Longitude)
+	}
+	if vehicle.Position.Speed == nil {
+		apiVehicle.Speed = 0
+	} else {
+		apiVehicle.Speed = float64(*vehicle.Position.Speed)
+	}
+
 	if trip == nil {
 		log.Printf("failed to find trip for vehicle ID: %s", vehicleId)
+	} else {
+		apiVehicle.RouteId = api.NewStringVehicleRouteId(trip.Route.Id)
+		apiVehicle.Direction = trip.DirectionId.String()
+		apiVehicle.Destination = trip.Headsign
 	}
 
-	bearing := *vehicle.Position.Bearing
-	latitude := *vehicle.Position.Latitude
-	longitude := *vehicle.Position.Longitude
-	var speed float32 = 0
-	if vehicle.Position.Speed != nil {
-		speed = *vehicle.Position.Speed
+	nextStop := getStop(vehicle, staticGtfs)
+	if nextStop != nil {
+		apiVehicle.Destination = nextStop.Name
+		if trip != nil {
+			lastStop := getStopBefore(*nextStop, *trip)
+			if lastStop != nil {
+				apiVehicle.LastStop = api.NewNilString(lastStop.Name)
+			}
+		}
+	} else {
+		log.Printf("failed to find next stop for vehicle ID: %s", vehicleId)
 	}
 
-	lastStop := api.NilString{Null: true}
-	directionId := trip.DirectionId.String()
-	destination := trip.Headsign
+	return apiVehicle
+}
 
-	return api.Vehicle{
-		ID:            api.NewStringVehicleID(vehicleId.ID),
-		RouteId:       api.NewStringVehicleRouteId(trip.Route.Id),
-		Direction:     directionId,
-		Heading:       int(bearing),
-		Longitude:     float64(longitude),
-		Latitude:      float64(latitude),
-		Speed:         float64(speed),
-		Destination:   destination,
-		DisplayStatus: "",
-		LastStop:      lastStop,
-		LastUpdated:   *vehicle.Timestamp,
+func getStop(vehicle gtfs.Vehicle, staticGtfs gtfs.Static) *gtfs.Stop {
+	if vehicle.StopID == nil {
+		return nil
 	}
+
+	for _, stop := range staticGtfs.Stops {
+		if stop.Id == *vehicle.StopID {
+			return &stop
+		}
+	}
+	return nil
+}
+
+func getStopBefore(stop gtfs.Stop, trip gtfs.ScheduledTrip) *gtfs.Stop {
+	sort.Slice(trip.StopTimes, func(i, j int) bool {
+		return trip.StopTimes[i].StopSequence < trip.StopTimes[j].StopSequence
+	})
+
+	for i, stopTime := range trip.StopTimes[1:] {
+		if stopTime.Stop.Id == stop.Id {
+			return trip.StopTimes[i].Stop
+		}
+	}
+	return nil
 }
