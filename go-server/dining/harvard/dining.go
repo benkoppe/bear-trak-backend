65297399a49451a084cfa573ba02271455930bdc
Ben <Koppe.Development@gmail.com>
2025-04-17 21:18:42 -0400

    Implement harvard dining

--- go-server/dining/harvard/dining.go
+++ go-server/dining/harvard/dining.go
@@ -1,8 +1,15 @@
 package harvard
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/benkoppe/bear-trak-backend/go-server/api"
 	"github.com/benkoppe/bear-trak-backend/go-server/dining/harvard/external"
+	"github.com/benkoppe/bear-trak-backend/go-server/dining/harvard/static"
+	"github.com/benkoppe/bear-trak-backend/go-server/dining/shared"
+	"github.com/benkoppe/bear-trak-backend/go-server/utils"
+	"github.com/benkoppe/bear-trak-backend/go-server/utils/time_utils"
 )
 
 type Cache = external.Caches
@@ -14,5 +21,151 @@ func InitCache(baseUrl, apiKey string) Cache {
 func Get(
 	externalCache Cache,
 ) ([]api.Eatery, error) {
-	return []api.Eatery{}, nil
+	externalRecipes, err := externalCache.RecipesCache.Get()
+	if err != nil {
+		return nil, fmt.Errorf("error fetching external recipes: %w", err)
+	}
+
+	staticEateries := static.GetEateries()
+
+	var eateries []api.Eatery
+
+	for _, staticEatery := range staticEateries {
+		newEatery := convertStatic(staticEatery, externalRecipes)
+		eateries = append(eateries, newEatery)
+	}
+
+	return eateries, nil
+}
+
+func convertStatic(static static.Eatery, externalRecipes []external.Recipe) api.Eatery {
+	eatery := api.Eatery{
+		ID:         static.ID,
+		Name:       static.Name,
+		NameShort:  static.Name,
+		ImagePath:  utils.ImageNameToPath("dining/harvard", static.ImageName),
+		Latitude:   static.Location.Latitude,
+		Longitude:  static.Location.Longitude,
+		Region:     static.Region,
+		PayMethods: []string{},
+		Categories: shared.ConvertStaticCategories(static.Categories),
+		Hours:      static.WeekHours.CreateFutureHours(),
+	}
+
+	if static.ApiNumber != nil {
+		eatery.NextWeekEvents = shared.SelectNextWeekEvents(collectFutureEvents(static, externalRecipes))
+	}
+
+	if static.AllWeekMenu != nil {
+		eatery.AllWeekMenu = convertStaticMenu(*static.AllWeekMenu)
+	}
+
+	return eatery
+}
+
+func collectFutureEvents(static static.Eatery, externalRecipes []external.Recipe) []api.EateryEvent {
+	if static.ApiNumber == nil {
+		return nil
+	}
+	locationNumber := *static.ApiNumber
+	weekHours := static.WeekHours
+
+	est := time_utils.LoadEST()
+	now := time.Now().In(est)
+
+	var events []api.EateryEvent
+
+	for i := 0; i < 7; i++ {
+		date := now.AddDate(0, 0, i)
+		hours := weekHours.GetHours(date)
+
+		for _, hour := range hours {
+			if hour.MealNumber != nil {
+				targetServeDate := date.Format("01/02/2006")
+				var recipes []external.Recipe
+
+				for _, recipe := range externalRecipes {
+					if recipe.LocationNumber == locationNumber && recipe.MealNumber == *hour.MealNumber && recipe.ServeDate == targetServeDate {
+						recipes = append(recipes, recipe)
+					}
+				}
+
+				menu := convertExternalRecipesToMenu(recipes)
+				start, err := hour.Open.ToDate(date)
+				if err != nil {
+					fmt.Printf("error converting open time: %v", err)
+					continue
+				}
+				end, err := hour.Close.ToDate(date)
+				if err != nil {
+					fmt.Printf("error converting close time: %v", err)
+					continue
+				}
+				event := api.EateryEvent{
+					Start:          start,
+					End:            end,
+					MenuCategories: menu,
+				}
+				events = append(events, event)
+			}
+		}
+	}
+
+	return events
+}
+
+func convertExternalRecipesToMenu(recipes []external.Recipe) []api.EateryMenuCategory {
+	grouped := make(map[string][]external.Recipe)
+
+	for _, recipe := range recipes {
+		name := recipe.MenuCategoryName
+		if _, ok := grouped[name]; !ok {
+			grouped[name] = []external.Recipe{}
+		}
+		grouped[name] = append(grouped[name], recipe)
+	}
+
+	var categories []api.EateryMenuCategory
+
+	for name, recipes := range grouped {
+		var items []api.EateryMenuCategoryItemsItem
+
+		for _, recipe := range recipes {
+			item := api.EateryMenuCategoryItemsItem{
+				Name:    recipe.RecipeName,
+				Healthy: false,
+			}
+			items = append(items, item)
+		}
+
+		category := api.EateryMenuCategory{
+			Name:  name,
+			Items: items,
+		}
+		categories = append(categories, category)
+	}
+
+	return categories
+}
+
+func convertStaticMenu(staticCategories []static.MenuCategory) []api.EateryMenuCategory {
+	var categories []api.EateryMenuCategory
+
+	for _, staticCategory := range staticCategories {
+		var items []api.EateryMenuCategoryItemsItem
+
+		for _, staticItem := range staticCategory.Items {
+			items = append(items, api.EateryMenuCategoryItemsItem{
+				Name:    staticItem.Item,
+				Healthy: staticItem.Healthy,
+			})
+		}
+
+		categories = append(categories, api.EateryMenuCategory{
+			Name:  staticCategory.Category,
+			Items: items,
+		})
+	}
+
+	return categories
 }
