e0566f990e04e4e9162fa993df105464a398b132
ben <koppe.development@gmail.com>
2026-02-05 13:42:12 -0500

    Extract otp gtfs definitions into separate gtfs-sources.nix

--- otp/flake.nix
+++ otp/flake.nix
@@ -54,22 +54,7 @@
                   '';
                 }
               )
-              {
-                cornell.gtfs."gtfs.zip" = {
-                  url = "https://realtimetcatbus.availtec.com/InfoPoint/GTFS-zip.ashx";
-                  sha256 = "sha256-oAvmvfqJxd3o2bcTFuJp5M+u9unIjkxKypPCJ7KahIA=";
-                };
-                harvard.gtfs = {
-                  "gtfs.zip".url = "https://passio3.com/harvard/passioTransit/gtfs/google_transit.zip";
-                  "gtfs.zip".sha256 = "sha256-EpMUc81e4ESTWzZeTNwjBzuOzGJlXILKq0pbPorBe6Q=";
-                  "gtfs-mbta.zip".url = "https://cdn.mbta.com/MBTA_GTFS.zip";
-                  "gtfs-mbta.zip".sha256 = "sha256-UYTv2cHiB4GbX+0vJCFOJb5oATwrnkgxoT8knI14o7Y=";
-                };
-                umich.gtfs."gtfs.zip" = {
-                  url = "https://webapps.fo.umich.edu/transit_uploads/google_transit.zip";
-                  sha256 = "sha256-9oT89xjuneNMXZbwA08RI/d7e7BsfI7AS+wuk0S/DKU=";
-                };
-              };
+              (import ./gtfs-sources.nix);
         in
         {
           devenv.shells.default =
---
+++ otp/gtfs-sources.nix
@@ -0,0 +1,33 @@
+{
+  cornell = {
+    gtfs = {
+      "gtfs.zip" = {
+        url = "https://realtimetcatbus.availtec.com/InfoPoint/GTFS-zip.ashx";
+        sha256 = "sha256-oAvmvfqJxd3o2bcTFuJp5M+u9unIjkxKypPCJ7KahIA=";
+      };
+    };
+  };
+
+  harvard = {
+    gtfs = {
+      "gtfs-mbta.zip" = {
+        url = "https://cdn.mbta.com/MBTA_GTFS.zip";
+        sha256 = "sha256-UYTv2cHiB4GbX+0vJCFOJb5oATwrnkgxoT8knI14o7Y=";
+      };
+      "gtfs.zip" = {
+        url = "https://passio3.com/harvard/passioTransit/gtfs/google_transit.zip";
+        sha256 = "sha256-EpMUc81e4ESTWzZeTNwjBzuOzGJlXILKq0pbPorBe6Q=";
+      };
+    };
+  };
+
+  umich = {
+    gtfs = {
+      "gtfs.zip" = {
+        url = "https://webapps.fo.umich.edu/transit_uploads/google_transit.zip";
+        sha256 = "sha256-9oT89xjuneNMXZbwA08RI/d7e7BsfI7AS+wuk0S/DKU=";
+      };
+    };
+  };
+
+}
